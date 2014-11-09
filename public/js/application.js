918d378897d089fa7366f63a217265ae522fc212
Michael Angelo <yamikamisama@gmail.com>
2014-11-09 17:32:22 -0500

    update sprite Bmage

--- public/js/application.js
+++ public/js/application.js
@@ -5,7 +5,7 @@ function preload() {
     game.load.image('sky', 'assets/world.png');
     game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/ruby.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+    game.load.spritesheet('dude', 'assets/bmage.png', 32, 48);
     game.load.image('diamond', 'assets/wind.png');
 
 }
