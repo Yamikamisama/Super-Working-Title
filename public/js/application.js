eb2d0e83c06fb4e14688c82987a3001717c18e0d
Michael Angelo <yamikamisama@gmail.com>
2014-11-10 00:03:37 -0500

    Level area update

--- public/js/application.js
+++ public/js/application.js
@@ -70,13 +70,13 @@ function create() {
     ground.body.immovable = true;
 
     //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
+    var ledge = platforms.create(500, 350, 'ground');
     ledge.body.immovable = true;
 
-    ledge = platforms.create(-150, 250, 'ground');
+    ledge = platforms.create(180, 150, 'ground');
     ledge.body.immovable = true;
 
-    ledge = platforms.create(500, 300, 'ground');
+    ledge = platforms.create(-150, 350, 'ground');
     ledge.body.immovable = true;
 
     playerOneView = setPlayer(playerOne.settings);
